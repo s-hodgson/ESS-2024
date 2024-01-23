56e9d2db4acccbac5ba912c4b56aff2f08f150b1
Sam Hodgson <60526476+s-hodgson@users.noreply.github.com>
2024-01-23 11:16:43 +0000

    Update main.c

--- ESS/Core/Src/main.c
+++ ESS/Core/Src/main.c
@@ -10,30 +10,8 @@ int main(void)
 	ess_helper_init();
 	// Set all the LEDs to on.
 
-
-	// Initialize the green LED.
-	void led_green_init(void);
-	// Turn the green LED on
-	void led_green_on(void);
-	// Turn the green LED off
-	void led_green_off(void);
-
-
 	*(uint32_t*)0x40020C14 = 0xF000;
-
-	void led_green_init() {
-		*(uint32_t*)0x40020C14 & 0x1000;
-	}
-	void led_green_on() {
-		*(uint32_t*)0x40020C14 | 0x1000;
-	}
-	void led_green_off() {
-		*(uint32_t*)0x40020C14 & 0x1000;
-	}
-
-	led_green_init();
-	led_green_on();
-
+	
     while(1){
     }
 }
